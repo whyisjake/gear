2a8d26afb71071b449cba231b54ababfe14ae68c
Jake Spurlock <whyisjake@gmail.com>
2013-09-21 15:28:15 -0700

    Condense this a bit.

--- inc/wp-cli/wp-cli.php
+++ inc/wp-cli/wp-cli.php
@@ -35,27 +35,30 @@ class GEAR_WP_CLI_Command extends WP_CLI_Command {
 				'post_author'   => 0,
 			);
 			$post_id = wp_insert_post( $post );
-			$title = get_the_title( $post_id );
-			WP_CLI::line('☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆');
-			if ( !$post_id ) {
-				WP_CLI::warning( "Couldn't insert post... Sorry about that." );
-			} else {
-				WP_CLI::success( "Inserted product: " . $title  );
+			if ( $post_id ) {
+				WP_CLI::line( get_the_title( $post_id ) );
 			}
-			$meta_id = update_post_meta( $post_id, 'url', esc_url( $product['url'] ) );
-			if ( !$meta_id ) {
-				WP_CLI::warning( "Didn't add the URL meta..." );
-			} else {
-				WP_CLI::success( "Added " . $product['url'] . " to " . $title );
-			}
-			$bitly = make_bitly_url( esc_url_raw ($product['url'] ) );
-			$bitlyurl = update_post_meta( $post_id, 'bitly_url', $bitly );
-			if ( !$bitlyurl ) {
-				WP_CLI::warning( "Didn't add the Bit.ly URL meta..." );
-			} else {
-				WP_CLI::success( "Added a shorturl of " . $bitly . " to " . $title );
-			}
-			WP_CLI::line('');
+			// $title = get_the_title( $post_id );
+			// WP_CLI::line('☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆');
+			// if ( !$post_id ) {
+			// 	WP_CLI::warning( "Couldn't insert post... Sorry about that." );
+			// } else {
+			// 	WP_CLI::success( "Inserted product: " . $title  );
+			// }
+			// $meta_id = update_post_meta( $post_id, 'url', esc_url( $product['url'] ) );
+			// if ( !$meta_id ) {
+			// 	WP_CLI::warning( "Didn't add the URL meta..." );
+			// } else {
+			// 	WP_CLI::success( "Added " . $product['url'] . " to " . $title );
+			// }
+			// $bitly = make_bitly_url( esc_url_raw ($product['url'] ) );
+			// $bitlyurl = update_post_meta( $post_id, 'bitly_url', $bitly );
+			// if ( !$bitlyurl ) {
+			// 	WP_CLI::warning( "Didn't add the Bit.ly URL meta..." );
+			// } else {
+			// 	WP_CLI::success( "Added a shorturl of " . $bitly . " to " . $title );
+			// }
+			// WP_CLI::line('');
 		}
 	}
 }
