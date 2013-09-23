1f9327be57d2cf0e784df9be32b89c80f06ab948
Jake Spurlock <whyisjake@gmail.com>
2013-09-23 06:38:41 -0700

    Drop the malicious code...

--- footer.php
+++ footer.php
@@ -28,8 +28,7 @@
 	<div class="container">
 		<div class="site-info wrap row">
 			<div class="fcred col-12">
-				Copyright &copy; <?php echo date('Y');?> <a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> - <?php bloginfo('description'); ?>.<br />
-				<?php fflink(); ?> | <a href="http://topwpthemes.com/<?php echo wp_get_theme(); ?>/" ><?php echo wp_get_theme(); ?> Theme</a> 	
+				Copyright &copy; <?php echo date('Y');?> <a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> - <?php bloginfo('description'); ?>.
 			</div>		
 
 		</div><!-- .site-info -->
